817626eff4e1e6c51a75626dce00dc344e307212
yuyue-xp <69074603+yuyue-xp@users.noreply.github.com>
2024-06-15 16:40:10 +0800

    1

--- src/mc_a.py
+++ src/mc_a.py
@@ -102,8 +102,8 @@ def welcome():
     print("此程序仅供学习研究，严禁用于其他用途！！！")
     print("解锁原理涉及修改系统文件，需使用管理员权限")
     print("解锁后可能会导致Microsoft Store异常..等！")
-    print("有能力请支持Minecraft正版！！！")
-    print("此工具仅支持Windows11，仅支持Windows11 ！")
+    print("禁止滥用，有能力请支持Minecraft正版！！！")
+    print("仅支持Windows11，仅支持Windows11 ！！！")
     print("Minecraft官网：https://minecraft.net")
     print("---------------------------------------")
 
